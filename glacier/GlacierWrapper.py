a10693f0face733b4738801ddab2bc4c810c3886
Jeff Alyanak <jeff@alyanak.ca>
2019-11-05 13:45:23 -0500

    Updated AWS Region Order
    Updated AWS regions to match order on https://docs.aws.amazon.com/general/latest/gr/rande.html#glacier_region

--- glacier/GlacierWrapper.py
+++ glacier/GlacierWrapper.py
@@ -114,46 +114,54 @@ class GlacierWrapper(object):
     MAX_VAULT_DESCRIPTION_LENGTH = 1024
     MAX_PARTS = 10000
     AVAILABLE_REGIONS = (
-            'us-east-1',
             'us-east-2',
+            'us-east-1',
             'us-west-1',
             'us-west-2',
+            'ap-east-1',
+            'ap-south-1',
+            'ap-northeast-3',
+            'ap-northeast-2',
+            'ap-southeast-1',
+            'ap-southeast-2',
+            'ap-northeast-1',
             'ca-central-1',
+            'cn-north-1',
+            'cn-northwest-1',
             'eu-central-1',
             'eu-west-1',
             'eu-west-2',
             'eu-west-3',
             'eu-north-1',
-            'ap-east-1',
-            'ap-northeast-1',
-            'ap-northeast-2',
-            'ap-northeast-3',
-            'ap-southeast-1',
-            'ap-southeast-2',
-            'ap-south-1',
             'me-south-1',
-            'sa-east-1')
+            'sa-east-1',
+            'us-gov-east-1',
+            'us-gov-west-1')
     AVAILABLE_REGIONS_MESSAGE = """\
 Invalid region. Available regions for Amazon Glacier are:
-us-east-1 US East (N. Virginia) 
-us-east-2 US East (Ohio) 
-us-west-1 US West (N. California) 
-us-west-2 US West (Oregon) 
-ca-central-1 Canada (Central) 
-eu-central-1 EU (Frankfurt) 
-eu-west-1 EU (Ireland) 
-eu-west-2 EU (London) 
-eu-west-3 EU (Paris) 
-eu-north-1 EU (Stockholm) 
+us-east-2 US East (Ohio)
+us-east-1 US East (N. Virginia)
+us-west-1 US West (N. California)
+us-west-2 US West (Oregon)
 ap-east-1 Asia Pacific (Hong Kong)
-ap-northeast-1 Asia Pacific (Tokyo) 
-ap-northeast-2 Asia Pacific (Seoul) 
-ap-northeast-3 Asia Pacific (Osaka-Local) 
-ap-southeast-1 Asia Pacific (Singapore) 
-ap-southeast-2 Asia Pacific (Sydney) 
-ap-south-1 Asia Pacific (Mumbai) 
+ap-south-1 Asia Pacific (Mumbai)
+ap-northeast-3 Asia Pacific (Osaka-Local)
+ap-northeast-2 Asia Pacific (Seoul)
+ap-southeast-1 Asia Pacific (Singapore)
+ap-southeast-2 Asia Pacific (Sydney)
+ap-northeast-1 Asia Pacific (Tokyo)
+ca-central-1 Canada (Central)
+cn-north-1 China (Beijing)
+cn-northwest-1 China (Ningxia)
+eu-central-1 EU (Frankfurt)
+eu-west-1 EU (Ireland)
+eu-west-2 EU (London)
+eu-west-3 EU (Paris)
+eu-north-1 EU (Stockholm)
 me-south-1 Middle East (Bahrain)
-sa-east-1 South America (São Paulo)\
+sa-east-1 South America (Sao Paulo)
+us-gov-east-1 AWS GovCloud (US-East)
+us-gov-west-1 AWS GovCloud (US-West)\
 """
 
     def setuplogging(self, logfile, loglevel, logtostdout):
